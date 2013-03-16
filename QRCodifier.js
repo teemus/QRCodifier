6984e20e684e6f56b93f19a4d9bc18ccb1fd390d
Sumeet Mulani <mail@sumeet.info>
2013-03-16 13:35:49 +0530

    Use the escape key to close the QR code panel.

--- QRCodifier.js
+++ QRCodifier.js
@@ -1,4 +1,8 @@
 (function(){
+    function removeQRCode() {
+        document.getElementById("QRCodifier").parentNode.removeChild(document.getElementById("QRCodifier"));
+    }
+
     if (document.getElementById("QRCodifier") !== null) {
         alert("Once should be good enough. ;)");
         return;
@@ -8,7 +12,7 @@
     qrCode.setAttribute("style","margin:7px 0px 0px 0px;display:inline;");
     var qrText = document.createElement("p");
     qrText.setAttribute("style", "text-align:center;");
-    qrText.innerHTML = "Click to close";
+    qrText.innerHTML = "Click (or press Esc key) to close";
 
     qrCode.setAttribute("title", "QR code for this website - Scan it with your smartphone now!");
     var qrCodeDiv = document.createElement("div");
@@ -17,10 +21,16 @@
     qrCodeDiv.setAttribute("id", "QRCodifier");
     var top = Math.max(window.innerHeight/2 - 130 + window.scrollY, 0);
     var left = Math.max(window.innerWidth/2 - 130, 0);
-    qrCodeDiv.setAttribute("onclick", "javascript:document.getElementById(\"QRCodifier\").parentNode.removeChild(document.getElementById(\"QRCodifier\"))");
+    qrCodeDiv.onclick = removeQRCode;
     qrCodeDiv.setAttribute("style", "background-color:#000;color:#fff;text-align:center;margin:0px;padding:0px;width:260px;height:260px;border:4px solid #e00;z-index:5000;position:absolute;top:" + top + "px;" + "left:" + left + "px;");
 
     var dBody = document.getElementsByTagName("body");
     dBody[0].insertBefore(qrCodeDiv, dBody[0].childNodes[0]);
     setTimeout(function(){ document.getElementById("QRCodifier").style.border="4px solid #000"; }, 550);
+    document.onkeydown = function(e) {
+        var evt = e || window.event;
+        if (evt.keyCode == 27) {
+            removeQRCode();
+        }
+    }
 })();
